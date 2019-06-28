192ce505836d730ad93787741416b01b181b8e4e
Daniel Spira <daniel.b.spira@gmail.com>
2019-06-28 13:43:01 -0700

    everything working properly except guesses so far resetting. still carries over last pressed letter from previous round

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,22 +1,29 @@
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z"]
 
+let alreadyGuessed = []
+let cpuChoice = randomLetter();
+
 document.onkeyup = function(event) {
     let userChoice = event.key;
-    let cpuChoice = letters[Math.floor(Math.random() * letters.length)];
     let guessesLeft = document.getElementById("guessesLeft");
+    alreadyGuessed.push(userChoice);
 
     
     if (userChoice === cpuChoice){
         document.getElementById("wins").innerText++;
-    } else {
-        document.getElementById("losses").innerText++;
-    }
+        guessesLeft.innerText = 10;
+        cpuChoice = randomLetter();
+        document.getElementById("guessesSoFar").innerText = "";
+    }  
 
     if (guessesLeft.innerText == 1) {
-        location.reload();
-    }
-
-    document.getElementById("guessesLeft").innerText--;
+        document.getElementById("losses").innerText++;
+        guessesLeft.innerText = 9;
+        cpuChoice = randomLetter();
+        document.getElementById("guessesSoFar").innerText = "";
+    } else {
+        document.getElementById("guessesLeft").innerText--;
+    }    
 
     let g = document.getElementById("guessesSoFar");
 
@@ -24,12 +31,11 @@ document.onkeyup = function(event) {
 
     g.innerText = preValue + " " + userChoice;
 
-
-
     console.log(userChoice);
     console.log(cpuChoice);
-    console.log(guessesLeft);
     
+}
 
-
+function randomLetter(){
+    return letters[Math.floor(Math.random() * letters.length)];
 }
